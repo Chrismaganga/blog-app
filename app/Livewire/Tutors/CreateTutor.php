77892fbdc1b58c884b9afaf11d99c89e0863949f
Chrismaganga <chrismaganga3@gmail.com>
2024-07-20 23:04:45 +0200

    first commit

---
+++ app/Livewire/Tutors/CreateTutor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Tutors;
+
+use Livewire\Component;
+
+class CreateTutor extends Component
+{
+    public function render()
+    {
+        return view('livewire.tutors.create-tutor');
+    }
+}
